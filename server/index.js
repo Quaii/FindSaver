0ec2cd1cc9433fafed0c659837e4f949fbf89066
Quai <Luis.dee16@gmail.com>
2025-05-10 00:08:46 +0200

    server

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/webscraper', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
